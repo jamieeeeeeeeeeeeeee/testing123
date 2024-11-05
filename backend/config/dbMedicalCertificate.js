19be1d6caecd1ce7d9403fe0410f8fe52798b044
jamie <jamie.tan.2023@scis.smu.edu.sg>
2024-11-05 14:06:23 +0800

    backend pushed

---
+++ backend/config/dbMedicalCertificate.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+require('dotenv').config({ path: '../secret.env' });
+
+var MEDICAL_CERTIFICATE_DB = process.env.MONGOURI_MEDICAL_CERTIFICATE_DB;
+
+const connectMedicalCertificateDB = () => {
+    return new Promise((resolve, reject) => {
+        const connection = mongoose.createConnection(MEDICAL_CERTIFICATE_DB);
+
+        connection.once('open', () => {
+            console.log('Connected to Medical Certificate Database on MongoDB Atlas');
+            resolve(connection);
+        });
+
+        connection.on('error', (err) => {
+            console.error('Error connecting to Medical Certificate Database:', err.message);
+            reject(err);
+        });
+    });
+};
+
+module.exports = connectMedicalCertificateDB;
